c11711b57ea26311efad32ba2a3ae8579fbe4837
Hackjack <jack03113@gmail.com>
2018-03-02 18:23:23 +0100

    Add examples

--- test.js
+++
@@ -1,5 +0,0 @@
-const gaap = require('./index.js');
-
-// console.log(JSON.stringify(gaap.getClass(1), null, 4));
-// console.log(JSON.stringify(gaap.getDescription(2116), null, 4));
-console.log(JSON.stringify(gaap.getHierarchy(29787), null, 4));
